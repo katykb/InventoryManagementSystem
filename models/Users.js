e29fcbc861e19d4cf670dfd9c71411495b462a27
Molly Niemeyer <99935249+mollygracemayar@users.noreply.github.com>
2022-05-28 14:02:46 -0500

    Add files via upload

---
+++ models/Users.js
@@ -0,0 +1,35 @@
+const {Model,Datatypes}=require('sequelize');
+const { User } = require('.');
+const sequelize=require('../config/connection');
+
+User.init(
+    {
+        category:{
+            type:Datatypes.STRING,
+            allowNull:false,
+            primaryKey:true
+
+        },
+        products:{
+            type:Datatypes.STRING,
+            allowNull:false,
+
+        },
+        Users:{
+            type:Datatypes.STRING,
+            allowNull:false,
+
+        },
+        sales:{
+            type:Datatypes.DECIMAL(10,2),
+            allowNull:false
+
+        },
+        sequelize,
+        timestamps:false,
+        freezeTableName:true,
+        underscored:true,
+        moduleName: 'user',
+    }
+)
+module.exports=Users;
