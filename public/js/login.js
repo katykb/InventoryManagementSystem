61cc74dfc4c1d812c97f2e7c687c537745d5d9b7
Katy Brown <katybr35@gmail.com>
2022-05-31 19:22:47 -0500

    adding the front end javascript

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,24 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
+  const userId = document.querySelector("#user-name-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-    const username = document.querySelector('#username-login').val.trim();
-    const password = document.querySelector('#password-login').val.trim();
+  if (userId && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ userId, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-    if (username && password){
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({username,password}),
-            headers: { 'Content-Type' : 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else{
-            alert('Please check username or password');
-        }
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in");
     }
+  }
 };
 
-
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
