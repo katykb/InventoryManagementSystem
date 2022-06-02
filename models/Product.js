8e17fee202e5ecb54cf9251910cb6b5eb35d3288
Ashley Albertson <ashleyalbertson51587@gmail.com>
2022-06-02 18:57:26 -0500

    Ashley's commit without homeRoutes.js

--- models/Product.js
+++ models/Product.js
@@ -11,7 +11,9 @@ Product.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        product_name: {
+    {
+        
+    }    product_name: {
             type: DataTypes.STRING,
             allowNull: false,
         },
