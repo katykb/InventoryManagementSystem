c7b265f029dee8d71b210205866fe041af23a8da
Katy Brown <katybr35@gmail.com>
2022-06-04 13:09:58 -0500

    inventory now in two pages

--- public/js/enterInventory.js
+++ public/js/enterInventory.js
@@ -5,7 +5,7 @@ const inventoryFormHandler = async (event) => {
   const media_type = document
     .querySelector("#mediaType-inventory")
     .value.trim();
-  const category_id = document.querySelector("#genre-inventory").value.trim();
+  const genre_type = document.querySelector("#genre-inventory").value.trim();
   const product_name = document.querySelector("#title-inventory").value.trim();
   const wholesale_price = document
     .querySelector("#wholesalePrice-inventory")
@@ -13,12 +13,13 @@ const inventoryFormHandler = async (event) => {
   const retail_price = document
     .querySelector("#retailPrice-inventory")
     .value.trim();
+    
 
   const response = await fetch("/api/inventory", {
     method: "POST",
     body: JSON.stringify({
       media_type,
-      category_id,
+      genre_type,
       product_name,
       wholesale_price,
       retail_price,
@@ -27,29 +28,12 @@ const inventoryFormHandler = async (event) => {
   });
 };
 
-const levelFormHandler = async (event) => {
-  event.preventDefault();
 
-  // const media_type = document.querySelector("#mediaType-inventory-levels").value.trim();
-  // console.log(media_type);
-  const genre_type = document.querySelector("#genreType-inventory-levels").value.trim();
-  window.location.href= `/inventory/${genre_type}`
-
-  // const getResponse = await fetch("/api/inventory/", {
-  //   method: "GET",
-  //   body: JSON.stringify({
-
-  //   })
-  // })
-
-}
 
 document
   .querySelector(".inventory-entry-form")
   .addEventListener("submit", inventoryFormHandler);
 
-document
-  .querySelector(".levels-form-group")
-  .addEventListener("submit", levelFormHandler);
+
 
 //Add route to push inventory to database
